06733bd0ef533ce9c5a79bb8b065fceccd92d3a1
Lipishree <lipishree.jena@gmail.com>
2017-04-09 21:17:43 +0530

    Update CAPSTONE.py

--- CAPSTONE.py
+++ CAPSTONE.py
@@ -3,13 +3,13 @@
 2.Fetch Historical data
 3.Identify Volatile stocks
 4.Device Short Term Trading startegy
-5Plot and display data
+5.Plot and display data
 @author: lipi
 """
-from CAP_STCK_ENTRY import stock_entry_option
-from STOCK_DATA import fetch_stock_hist_data
-from OUTLIER_STOCKS import get_volatile_stocks
-from SIGNAL_FILTERS import mean_reversion_and_trading_strategy
+from CAP_STCK_ENTRY  import stock_entry_option
+from STOCK_DATA      import fetch_stock_hist_data
+from OUTLIER_STOCKS  import get_volatile_stocks
+from SIGNAL_FILTERS  import mean_reversion_and_trading_strategy
 from PLOT_INDICATORS import plot_signals_and_indicators
     
 if __name__ == '__main__':
