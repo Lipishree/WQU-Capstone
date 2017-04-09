f6a0df8a112bf3b2f98cb68266e3d2c1175574d2
Lipishree <lipishree.jena@gmail.com>
2017-04-09 23:01:29 +0530

    Add files via upload

--- STOCK_DATA.py
+++ STOCK_DATA.py
@@ -1,15 +1,15 @@
 """
 Get historical data of stocks
+from Yahoo Finanace using the tickers entered
 @author: lipi
 """
 import datetime
 from pandas.io import data
 
-def fetch_stock_hist_data(stocks):
-    #Enter the stocks you wish to use 
+def fetch_stock_hist_data(stocks): 
     date1 = datetime.date.today() #today
     end= date1
-    start = date1.replace(year=date1.year - 1)
+    start = date1.replace(year=date1.year - 1)#Fetch the data of past one year
     start_str = '{:%Y%m%d}'.format(start)
     end_str = '{:%Y%m%d}'.format(end)
     all_data = {}
@@ -17,6 +17,7 @@ def fetch_stock_hist_data(stocks):
     for ticker in stocks:
         i+=1
         try:
+            #Get data from Yahoo Finance
             all_data[ticker] = data.get_data_yahoo(ticker,start_str,end_str)   
         except IOError:
             continue
